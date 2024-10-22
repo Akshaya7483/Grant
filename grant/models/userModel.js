c15bac72b218c77854c9976025dcb57adf55072b
Akshaya7483 <akshayas.bsc23@rvu.edu.in>
2024-10-22 16:17:26 +0530

    committed 10/22/2024, 4:17:26 PM

--- grant/models/userModel.js
+++ grant/models/userModel.js
@@ -1,5 +1,13 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./database.sqlite');
+db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT UNIQUE NOT NULL,
+      email TEXT UNIQUE NOT NULL,
+      password TEXT NOT NULL
+    )
+  `);
 
 module.exports = {
   createUser: (username, email, password, callback) => {
